bac31e1ae74fa4c622bf873b5f2acbee53315319
Dmitri <d@eurekaking.com>
2017-01-19 12:53:18 +0700

    Change default server port to 3001

--- config/boot.rb
+++ config/boot.rb
@@ -1,3 +1,12 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
+
+# Start on port 3001
+require 'rails/commands/server'
+module DefaultOptions
+  def default_options
+    super.merge!(Port: 3001)
+  end
+end
+Rails::Server.send(:prepend, DefaultOptions)
