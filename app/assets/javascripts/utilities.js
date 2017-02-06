16829f0a353c28febf08d3a2784d340535fbe2f4
Dmitri <d@eurekaking.com>
2017-02-06 21:16:22 +0700

    - Removed unused files - Refactored (heavily) pointer events JS for index page - Added "unread" badge for favorites - Quote on the homepage has a prettier opaque background

---
+++ app/assets/javascripts/utilities.js
@@ -0,0 +1,23 @@
+function __q(q){
+	return document.querySelectorAll(q);
+}
+function __each(el, f){
+	for (var i = 0; i < el.length; i++) {
+		f(i);
+	}
+}
+/* 
+function __parent(el, selector, stopSelector) {
+  var retval = null;
+  while (el) {
+    if (el.matches(selector)) {
+      retval = el;
+      break
+    } else if (stopSelector && el.matches(stopSelector)) {
+      break
+    }
+    el = el.parentElement;
+  }
+  return retval;
+}
+*/
