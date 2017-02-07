a2a144cee3a5efac7d5d4fc8b5987a8656edc95e
Dmitri <d@eurekaking.com>
2017-02-07 11:29:44 +0700

    Added formatting function and styles for undesired blockquote paragraph lengths

--- app/assets/javascripts/toc-pointer-events.js
+++ app/assets/javascripts/toc-pointer-events.js
@@ -3,12 +3,12 @@ function tocPointerEvents(){
 	var tocLi = __q('.table-of-contents li, .table-of-contents--feature > section');
 	
 	// util: add or remove "active" class to parent li or featured section element
-	function _upActive(el, b){ e = el.parentElement.parentElement.classList; b? e.add("active") : e.remove("active"); }
+	function _upActive(el, b){ e = el.parentElement.parentElement.classList; b? e.add("active") : e.remove("active"); };
 	// util: add event listener and perform _upActive()
-	function _ua1(el, event){ el.addEventListener(event, function(){ _upActive(this, true); }); }
-	function _ua0(el, event){ el.addEventListener(event, function(){ _upActive(this, false); }); }
+	function _ua1(el, event){ el.addEventListener(event, function(){ _upActive(this, true); }); };
+	function _ua0(el, event){ el.addEventListener(event, function(){ _upActive(this, false); }); };
 	// util: check if element exists
-	function _is(el){ if(el !== undefined) return true; }
+	function _is(el){ if(el !== undefined) return true; };
  
  	// touchstart-end
  	__each(tocLi, function(i){
@@ -28,4 +28,4 @@ function tocPointerEvents(){
  		});
 	});
 	
-}
+};
