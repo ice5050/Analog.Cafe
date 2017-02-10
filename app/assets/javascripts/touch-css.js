1204d62bb00cc0536e89d69ee45ccc67327cdefa
Dmitri <d@eurekaking.com>
2017-02-10 17:04:31 +0700

    - Fixed pinned tab SVG for safari - Modified tap interface so that downstates do not obstract scrolling on touch screens - Added soft hyphens to "Photography" and included it as feature for release

---
+++ app/assets/javascripts/touch-css.js
@@ -0,0 +1,26 @@
+
+
+
+(function(){
+	isScrolling = false;
+	tapVerifying = false;
+	tapVerifyDelay = 15;
+
+	// Hide :active classes (via SASS, operated via .no-tap) while isScrolling
+	window.addEventListener("scroll", function(){
+		__q('body')[0].classList.add("no-tap");
+		isScrolling = true;
+	});
+	window.addEventListener("doneScrolling", function(){
+		isScrolling = false;
+		if(!tapVerifying)__q('body')[0].classList.remove("no-tap");
+	});
+
+	// Wait adding :active class to see if user is about to drag/scroll
+	var tapTimer = -1;
+	window.addEventListener("touchstart", function(){
+		tapVerifying = true;
+		if (tapTimer != -1) clearTimeout(tapTimer);
+		tapTimer = window.setTimeout("if(!isScrolling)__q('body')[0].classList.remove('no-tap');tapVerifying=false;", tapVerifyDelay);
+	});
+})();
