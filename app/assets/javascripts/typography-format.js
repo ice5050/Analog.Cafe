8a60ed1a331b78ac01008cb0fcfface10b7633f6
Dmitri <d@eurekaking.com>
2017-02-16 01:04:33 +0700

    Re-designed the flow and layout for the index page

--- app/assets/javascripts/typography-format.js
+++ app/assets/javascripts/typography-format.js
@@ -2,4 +2,13 @@ function typographyFormat(){
 	__q("blockquote p:first-child").forEach(function(el) {
 		(el.innerHTML.length < 175 ) ? el.parentElement.classList.add("short") : false;
 	});
+	__q(".toc-chartrim").forEach(function(el) {
+		trim = 220;
+		textLength = el.innerHTML.length;
+		ellipsis = (textLength >= trim) ? '...' : '';
+		trimmed = el.innerHTML.substr(0, trim);
+		el.innerHTML = (textLength >= trim) ? trimmed.substr(0, Math.min(trimmed.length, trimmed.lastIndexOf(" "))) + ellipsis : el.innerHTML;
+		el.innerHTML = (trimmed == '') ? '...' : el.innerHTML;
+	});
 };
+document.addEventListener('DOMContentLoaded', typographyFormat);
