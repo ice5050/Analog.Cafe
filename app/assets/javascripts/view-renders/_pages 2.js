50e45b02c573b6efd930aa20615557d8cbd92ebc
Dmitri <d@eurekaking.com>
2017-02-25 14:43:40 +0700

    Added rendered assets, fixed header menu (switched to flexbox from table as well)

---
+++ app/assets/javascripts/view-renders/_pages 2.js
@@ -0,0 +1,34 @@
+
+
+
+// list of special functions per page:
+pageAction = new __List({
+	"index": 			function(){
+		var analogCoffee = document.querySelector('.logo-container');
+		analogCoffee.classList.add("-view");
+	},
+	"favorites":	function(){
+		__q(".table-of-contents li").forEach(function(el) {
+			el.innerHTML += '<div class="toc-favorite"><a href="#" class="favorite -active"></a></div>';
+		});
+		__q(".button-favorites").forEach(function(el) { el.classList.add("-view"); });
+	},
+	"submit": function(){
+		__q(".button-submit").forEach(function(el) { el.classList.add("-view"); });
+	},
+	"articles": function(){
+		__q(".button-articles").forEach(function(el) { el.classList.add("-view"); });
+	},
+	"essays": function(){
+		__q(".button-essays").forEach(function(el) { el.classList.add("-view"); });
+	}
+});
+
+
+
+
+// get page id and execute
+function pageActionExecute(){
+	currentAction = __q("body")[0].getAttribute('data-controller-action');
+	pageAction.select(currentAction);
+}
