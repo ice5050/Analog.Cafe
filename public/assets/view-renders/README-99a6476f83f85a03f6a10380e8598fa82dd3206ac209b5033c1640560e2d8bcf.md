e0c351dae0a323f5fe52dc8ef189714137c34205
Dmitri <d@eurekaking.com>
2017-02-26 20:41:13 +0700

    Rebuilt assets, updated bower componetns

---
+++ public/assets/view-renders/README-99a6476f83f85a03f6a10380e8598fa82dd3206ac209b5033c1640560e2d8bcf.md
@@ -0,0 +1,5 @@
+Scripts in this folder are automatically executed on DOM Ready event.
+
+They also need to be invoked every time a new view is loaded (new URL or major page component, like article list or article text).
+
+To invoke all scripts call `parseAllViews()` (from render.js)
--- vendor/assets/bower_components/active-touch/touch.js
+++ vendor/assets/bower_components/active-touch/touch.js
@@ -12,14 +12,14 @@ var touchRespond = {
 
 	// remove all other active states on the document
 	reset: function(){
-		__each(__q('a'), function(el) {
-			el.classList.remove("-active");
+		__q('a').forEach(function(el) {
+				el.classList.remove("-active");
 		});
 	},
 	
 	// add events to all elements on page
 	init: function(){
-		__each(__q('a'), function(el) {
+		__q('a').forEach(function(el) {
 		
 			var pointerDown = false;
 			var activeAvailable = true;
