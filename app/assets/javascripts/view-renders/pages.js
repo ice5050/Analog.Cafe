d87e26b4dea20b4431de2adbfbc9a15400493883
Dmitri <d@eurekaking.com>
2017-02-26 20:26:54 +0700

    Fixes for IE - Polyfill for Promise (IE) - Shortcut for forEach function for accessibility on most browsers - Polyfill for fireEvent (IE) - Update components

--- app/assets/javascripts/view-renders/pages.js
+++ app/assets/javascripts/view-renders/pages.js
@@ -7,7 +7,7 @@ pageAction = new __List({
 	},
 	"favorites":	function(){
 		var heartButton = '<div class="toc-favorite"><a href="#" class="favorite -active"></a></div>';
-		__q(".table-of-contents li").forEach(function(el) {
+		__each(__q(".table-of-contents li"), function(el) {
 			el.innerHTML.includes(heartButton) ? false :
 			el.innerHTML += '<div class="toc-favorite"><a href="#" class="favorite -active"></a></div>';
 		});
@@ -20,12 +20,9 @@ pageAction = new __List({
 	}
 });
 
-
-
-
 // get page id, display active state on all related buttons and execute special functions:
 function pageActionExecute(){
 	currentAction = __q("body")[0].getAttribute('data-controller-action');
-	__q(".button-" + currentAction).forEach(function(el) { el.classList.add("-view"); });
+	__each(__q(".button-" + currentAction), function(el) { el.classList.add("-view"); });
 	pageAction.select(currentAction);
 }
