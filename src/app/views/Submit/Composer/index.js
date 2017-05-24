8e9ea52a654d2dba7ae03971f77040fb7a7f1b46
Dmitri <d@eurekaking.com>
2017-05-24 15:30:22 +0700

    Removed Draft.js

--- src/app/views/Submit/Composer/index.js
+++ src/app/views/Submit/Composer/index.js
@@ -1,6 +1,5 @@
 // tools
 import React from "react"
-import { Editor, EditorState, RichUtils } from "draft-js"
 
 // components
 import { Header } from "../../../components/Composer"
@@ -10,28 +9,6 @@ import { Wrapper } from "./styles"
 
 // render
 export class Composer extends React.Component {
-	constructor(props) {
-    super(props)
-    this.state = { editorState: EditorState.createEmpty() }
-    this.onChange = (editorState) => this.setState({ editorState })
-    this.handleKeyCommand = this.handleKeyCommand.bind(this)
-  }
-  handleKeyCommand(command) {
-    const newState = RichUtils.handleKeyCommand(this.state.editorState, command)
-    if (newState) {
-      this.onChange(newState)
-      return "handled"
-    }
-    return "not-handled"
-  }
-  
-  makeBold() {
-		this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, "BOLD"))
-	}
-	makeItalic() {
-		this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, "ITALIC"))
-	}
-  
 	render() {
 		return (
 			<Article>
@@ -44,15 +21,9 @@ export class Composer extends React.Component {
 				<Section>
 					<span onClick={ this.makeBold.bind(this) }>[Bold]</span> <span onClick={ this.makeItalic.bind(this) }>[Italic]</span>
 
-					<Wrapper><Editor
-						editorState={ 			this.state.editorState } 
-						onChange={ 					this.onChange }
-						handleKeyCommand={	this.handleKeyCommand }
+					<Wrapper>
 						
-						placeholder="Start typing..."
-						spellCheck={ 				true }
-						ref="editor"
-					/></Wrapper>
+					</Wrapper>
 					
 				</Section>
 			</Article>
--- src/app/views/Submit/Composer/styles.js
+++ src/app/views/Submit/Composer/styles.js
@@ -3,15 +3,15 @@ import styled from "styled-components"
 
 // css
 export const Wrapper = styled.div`
-	.public-DraftEditorPlaceholder-root {
-		position: 	absolute;
-		z-index: 		${ props => props.theme.layer.tuck };
-		opacity: 		${ props => props.theme.opacity.half };
-	}
-	.DraftEditorPlaceholder-hidden { display: none; }
-	.public-DraftEditor-content {
-		height: 							100vh;
-		-webkit-user-modify: 	read-write-plaintext-only; // remove iOS edit buttons
-		user-modify: 					read-write-plaintext-only; // remove iOS edit buttons
-	}
+// 	.public-DraftEditorPlaceholder-root {
+// 		position: 	absolute;
+// 		z-index: 		${ props => props.theme.layer.tuck };
+// 		opacity: 		${ props => props.theme.opacity.half };
+// 	}
+// 	.DraftEditorPlaceholder-hidden { display: none; }
+// 	.public-DraftEditor-content {
+// 		height: 							100vh;
+// 		-webkit-user-modify: 	read-write-plaintext-only; // remove iOS edit buttons
+// 		user-modify: 					read-write-plaintext-only; // remove iOS edit buttons
+// 	}
 `
