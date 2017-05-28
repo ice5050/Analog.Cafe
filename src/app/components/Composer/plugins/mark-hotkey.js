817c710cff3ed574d301db345d318ca3824655fb
Dmitri <d@eurekaking.com>
2017-05-28 12:55:04 +0700

    Added back line break function for block escape plugin

---
+++ src/app/components/Composer/plugins/mark-hotkey.js
@@ -0,0 +1,25 @@
+// tools
+import keycode from "keycode"
+
+// return
+// *first plugin* (hotkeys)
+export function PluginMarkHotkey(options) {
+  const { type, key } = options
+
+  // Return our "plugin" object, containing the `onKeyDown` handler.
+  return {
+    onKeyDown(event, data, state) {
+      // Check that the key pressed matches our `code` option.
+      if (!event.metaKey || keycode(event.which) !== key) return
+
+      // Prevent the default characters from being inserted.
+      event.preventDefault()
+
+      // Toggle the mark `type`.
+      return state
+        .transform()
+        .toggleMark(type)
+        .apply()
+    }
+  }
+}
