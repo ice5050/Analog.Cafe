b42bd02a01ae3b81eaa800458189e36fe8c7f2d8
Dmitri <d@eurekaking.com>
2017-06-05 01:24:10 +0700

    Title and subtitle are saved in local storage

--- src/app/components/Composer/components/Header/index.js
+++ src/app/components/Composer/components/Header/index.js
@@ -1,5 +1,7 @@
 // tools
 import React from "react"
+import { saveHeader } from "../../helpers/saver"
+import { loadHeader } from "../../helpers/loader"
 import { connect } from "react-redux"
 
 // components
@@ -14,17 +16,22 @@ import { PageHeader, PageByline } from "../../../Article/styles/header"
 
 
 // return
-class ComposerHeader extends React.Component {
+let headerData = {}
+export default class ComposerHeader extends React.Component {
 	constructor(props) {
     super(props)
     this.handleTitleChange = this.handleTitleChange.bind(this)
     this.handleSubtitleChange = this.handleSubtitleChange.bind(this)
   }
   handleTitleChange(event) {
-    this.props.changeTitle(event)
+  	headerData.title = event
+  	saveHeader(headerData)
+    //this.props.changeTitle(event)
   }
   handleSubtitleChange(event) {
-    this.props.changeSubtitle(event)
+  	headerData.subtitle = event
+  	saveHeader(headerData)
+    //this.props.changeSubtitle(event)
   }
   render() {
 		return (
@@ -32,30 +39,32 @@ class ComposerHeader extends React.Component {
 				<PageTitle
 					placeholder="Write Your Title Here"
 					onChange={ this.handleTitleChange }
+					value={ loadHeader().title }
 				></PageTitle>
 				<PageSubtitle
 					placeholder="Subtitle (Optional)"
 					onChange={ this.handleSubtitleChange }
+					value={ loadHeader().subtitle }
 				></PageSubtitle>
 				<PageByline>Your name & profile link will appear here.</PageByline>
 			</PageHeader>
 		)
 	}
 }
-const mapStateToProps = (state) => {
-	return {
-		title: state.title,
-		subtitle: state.subtitle
-	}
-}
-const mapDispatchToProps = (dispatch) => {
-	return {
-		changeTitle: (text) => {
-			dispatch(changeTitle(text))
-		},
-		changeSubtitle: (text) => {
-			dispatch(changeSubtitle(text))
-		}
-	}
-}
-export default connect(mapStateToProps, mapDispatchToProps)(ComposerHeader)
+// const mapStateToProps = (state) => {
+// 	return {
+// 		title: state.title,
+// 		subtitle: state.subtitle
+// 	}
+// }
+// const mapDispatchToProps = (dispatch) => {
+// 	return {
+// 		changeTitle: (text) => {
+// 			dispatch(changeTitle(text))
+// 		},
+// 		changeSubtitle: (text) => {
+// 			dispatch(changeSubtitle(text))
+// 		}
+// 	}
+// }
+// export default connect(mapStateToProps, mapDispatchToProps)(ComposerHeader)
