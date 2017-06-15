9dbd380a307d60df41957188acc345606827996e
Dmitri <d@eurekaking.com>
2017-06-15 16:56:29 +0700

    Fixed warning

--- src/app/components/Buttons/components/SmartLink/index.js
+++ src/app/components/Buttons/components/SmartLink/index.js
@@ -4,6 +4,7 @@ import { Link } from "react-router"
 
 // styles
 export const SmartLink = props => {
-	return props.to.includes("http") ? <a href={props.to} target="_blank" rel="nofollow noopener noreferrer" {...props} />
-	: <Link {...props}>{props.children}</Link>
+	return props.to.includes("http") 
+		? <a href={props.to} target="_blank" rel="nofollow noopener noreferrer" {...props}>{ props.children }</a>
+		: <Link {...props}>{props.children}</Link>
 }
