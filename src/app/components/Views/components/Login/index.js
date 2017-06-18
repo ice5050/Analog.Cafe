a636e1d3ffe584fe18402647603d0d287c963f35
Dmitri <d@eurekaking.com>
2017-06-19 00:34:30 +0700

    Refactored Login page

---
+++ src/app/components/Views/components/Login/index.js
@@ -0,0 +1,34 @@
+// tools
+import React from "react"
+
+// components
+import Heading from "../../../ArticleHeading"
+import Button from "../../../Button"
+import { Article, Section } from "../../../ArticleStyles"
+
+
+// styles
+import { TwitterButton, FacebookButton } from "./styles"
+
+
+// render
+export default props => {
+	return(
+		<Article>
+			<Heading pageTitle="Log In" />
+			<Section>
+
+				<TwitterButton to="/">Login with Twitter</TwitterButton>
+
+				<p style={{textAlign: "center"}}><em>- or -</em></p>
+
+				<FacebookButton to="/">Login with Facebook</FacebookButton>
+
+				<p style={{textAlign: "center"}}><em>- or -</em></p>
+				<h3>Login with Email</h3>
+
+				<Button to="/">Cancel</Button>
+			</Section>
+		</Article>
+	)
+}
---
+++ src/app/components/Views/components/Login/styles.js
@@ -0,0 +1,37 @@
+// tools
+import React from "react"
+import styled, { css } from "styled-components"
+
+// images
+import facebookLogo from "./images/logo-facebook.svg"
+import twitterLogo from "./images/logo-twitter.svg"
+
+
+// components
+import Button from "../../../Button"
+
+// css
+const brandButtonStyles = css`
+	color: 		${ props => props.theme.color.background } !important;
+	img {
+		height: 2em;
+	}
+`
+const StyledTwitterButton = styled(Button)`
+	background-color:	#1da1f2;
+	${ brandButtonStyles }
+	img {
+		margin: -1.25em 0 -1em -1em;
+	}
+`
+
+const StyledFacebookButton = styled(Button)`
+	background-color:	#3c5a99;
+	${ brandButtonStyles }
+	img {
+		margin: -1.5em .25em -1em -1em;
+	}
+`
+
+export const TwitterButton = props => <StyledTwitterButton><img src={ twitterLogo } alt="Twitter logo"/> { props.children }</StyledTwitterButton>
+export const FacebookButton = props => <StyledFacebookButton><img src={ facebookLogo } alt="Facebook logo"/> { props.children }</StyledFacebookButton>
