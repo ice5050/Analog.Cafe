df7c9a56d5e6df46bb50544ee6e8a2cb2194c585
Dmitri <d@eurekaking.com>
2017-06-20 14:04:07 +0700

    Go back button uses browser history

--- src/app/components/Views/Login/index.js
+++ src/app/components/Views/Login/index.js
@@ -1,10 +1,12 @@
 // tools
 import React from "react"
+import { browserHistory } from "react-router"
 
 // components
 import Heading from "../../ArticleHeading"
 import Button from "../../Button"
 import { Article, Section } from "../../ArticleStyles"
+import { SubtitleInput } from "../../InputText"
 
 
 // styles
@@ -18,15 +20,18 @@ export default props => {
 			<Heading pageTitle="Log In" />
 			<Section>
 
-				<TwitterButton to="/">Login with Twitter</TwitterButton>
+				<TwitterButton to="/submit/send">Login with Twitter</TwitterButton>
 
-				<p style={{textAlign: "center"}}><em>- or -</em></p>
+				<p style={{ textAlign: "center" }}><em>- or -</em></p>
 
-				<h3 style={{textAlign: "center", padding: 0}}>type@your.email</h3>
-				<Button to="/" red>Login with Email</Button>
+				<SubtitleInput
+					style={{ textAlign: "center", padding: 0 }}
+					placeholder="type@your.email"
+				></SubtitleInput>
+				<Button to="/submit/send" red>Login with Email</Button>
 
-				<p style={{textAlign: "center"}}><em>- or -</em></p>
-				<Button to="/">Cancel</Button>
+				<p style={{ textAlign: "center" }}><em>- or -</em></p>
+				<Button to="/" onClick={ browserHistory.goBack }>Go Back</Button>
 			</Section>
 		</Article>
 	)
