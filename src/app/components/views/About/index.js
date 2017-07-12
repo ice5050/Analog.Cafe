62c130defa883382a7484808b63c372becf83792
Dmitri <d@eurekaking.com>
2017-07-12 22:22:46 +0700

    About page done.

--- src/app/components/views/About/index.js
+++ src/app/components/views/About/index.js
@@ -30,20 +30,15 @@ export default props => {
 				pageSubtitle="Film Photography Publication"
 			/>
 			<Section endsign>
-				<Figure src="/images/figures/about-chiangmai-8.jpg" feature>A lamp that lights the work table with the laptop, notebook and camera used to build this website. Shot on Ilford PAN 400, pushed to 1600.</Figure>
-
+				<Figure src="/images/figures/about-forest.jpg" feature>A short ride up a twisty mountain road from downtown Chiang Mai (where Analog.Cafe is built) is a tropical forest.</Figure>
 				<blockquote><strong>Analog.Cafe</strong> exists because of the people who contribute their art and the editors who curate it. Because of the developers and designers who contribute their time and skill to build this platform. And because of the 51 people who have graciously <Link to="https://www.kickstarter.com/projects/dmitrizzle/analogcafe-the-remarkable-film-photography-blog">funded</Link> the initial stages of this project.</blockquote>
 				<p><strong>Analog.Cafe</strong> is a curated film photography publication that encourages and supports collaboration.</p>
-				<p>This project is a selection of creative & informative works based on quality, creativity and diversity.</p>
-
-				<Figure src="/images/figures/about-chiangmai.jpg" feature>An overcast view of Doi Suthep mountain and Chiang Mai city at the foot of it. Shot on expired Macro Eagle TCS 400 color surveilance film.</Figure>
-				<p><strong>Analog.Cafe</strong> began as an idea inspired by collaboration and shared passion for creativity. It is a product of past, present and future technology &mdash; and ideas.</p>
-				<p>This project is initiated and built in-part by <em>dmitrizzle</em> from the tropical mountain city of Chiang Mai (Thailand).</p>
+				<p><strong>Analog.Cafe</strong> favours creative & informative works based on quality, creativity and diversity.</p>
 
 				<hr />
 
-				<h3>Thank You Kickstarter Backers</h3>
-				<p>The individuals in this list are the first to offer their love and support. A huge round of applause and a low bow goes to this noble group! <span role="img" aria-label="Clapping hands and a bowing person">👏🙇</span></p>
+				<h3>Thank You, Kickstarter Backers!</h3>
+				<p>The individuals in this list were the first to offer their love and support. <span role="img" aria-label="Clapping hands and a bowing person">👏🙇</span></p>
 
 				<ThankYouList>
 					Thayanantha Thevanayagam<br />
@@ -98,6 +93,7 @@ export default props => {
 					Hakan Alca<br />
 					Ben Cairns
 				</ThankYouList>
+
 
 				<h3>Connect, Contact & Get Involved</h3>
 				<p>If you have a question, suggestion or just want to chat, feel free to follow and message on <Link to="https://twitter.com/analog_cafe">Twitter</Link>, <Link to="https://facebook.com/analog8cafe">Facebook</Link> or <Link to="https://instagram.com/analog_cafe">Instagram</Link>. Otherwise you can email <strong>d (at) analog (dot) cafe</strong>.</p>
