7eeb54550b1d043eb8d5eec740a1450082615615
Dmitri <d@eurekaking.com>
2017-07-14 14:39:37 +0700

    Updated image & save document text

--- src/app/containers/Composer/helpers/saver.js
+++ src/app/containers/Composer/helpers/saver.js
@@ -5,8 +5,10 @@ import throttle from "lodash/throttle"
 
 // return
 export const saveContent = throttle((document, state) => {
+	console.log(document)
 	const contentState = JSON.stringify(Raw.serialize(state))
 	localStorage.setItem("composer-content-state", contentState)
+	localStorage.setItem("composer-content-text", document.text)
 }, 3000)
 
 export const saveHeader = throttle(header => {
