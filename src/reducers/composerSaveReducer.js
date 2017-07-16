eeac43c9de0a1cbbe5b5e7b5eb9368d21208a93a
Dmitri <d@eurekaking.com>
2017-07-16 17:54:22 +0700

    Actions, reducers

---
+++ src/reducers/composerSaveReducer.js
@@ -0,0 +1,18 @@
+const composerSaveReducer = (state = {
+		status: "Draft",
+	}, action) =>  {
+	switch (action.type) {
+		case "UPDATE_STATUS":
+			state = {
+				...state,
+				status: action.payload
+			}
+			break
+		default:
+			state = { ...state }
+	}
+	console.log(state)
+	return state
+};
+
+export default composerSaveReducer
