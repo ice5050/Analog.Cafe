f7f1bcd416d9f37e1ae2ff0d5bbe697598ead58e
Dmitri <d@eurekaking.com>
2017-07-17 16:15:28 +0700

    Merged /components/Modal into index.js

--- src/app/containers/Modal/index.js
+++ src/app/containers/Modal/index.js
@@ -1,11 +1,14 @@
 // tools
 import React from "react"
+import axios from "axios"
 
 // redux
 import { connect } from "react-redux"
+import defaultPostState from "./state.json"
+
 
 // components
-import Fetch from "./containers/Fetch"
+import Card from "./containers/Card"
 
 // styles
 import { Wrapper } from "./styles"
@@ -26,6 +29,49 @@ class Modal extends React.Component {
     )
   }
 }
+
+class Fetch extends React.Component {
+	state = this.props.fetch
+
+  _fetch = () => {
+  	let slug = this.props.fetch
+		// fetch & update state
+		if(this.state.slug === slug) return
+		axios({
+		  method: 			this.props.method || "get",
+		  url: 					slug + ".json",
+		  data:					this.props.data || "",
+		})
+			.then(response => {
+				let data = response.data
+				this.setState({
+					title:				data.title,
+					image:				data.image || this.props.image,
+					text:					data.text,
+					slug,
+					buttons: 			data.buttons,
+				})
+			})
+			.catch(error => console.log(error))
+  }
+
+  componentDidMount = () => this._fetch()
+  componentDidUpdate = () => this._fetch()
+	// need condition for componentWillUnmount()
+
+  render() {
+		return(
+			<Card
+				title={ this.state.title  }
+				image={ this.state.image }
+				text={ this.state.text }
+				buttons={ this.state.buttons }
+				show={ this.props.show }
+			/>
+		)
+	}
+}
+
 
 
 // connet with redux
