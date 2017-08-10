8693462909752244c6bdd57491bc5da86216a1c1
Dmitri <d@eurekaking.com>
2017-08-10 20:34:46 +0700

    New section labels & refactor constants

--- src/constants/app.js
+++ src/constants/app.js
@@ -1,2 +1,3 @@
 export const DOMAIN_NAME = "analog.cafe"
-export const APP_DESCRIPTION = "Analog (film) photography publication"
+export const APP_NAME = "Analog.Cafe"
+export const APP_DESCRIPTION = "A film photography publication"
