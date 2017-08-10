36553924b1d840b11b0058a26a78aac9e9227f2d
Dmitri <d@eurekaking.com>
2017-08-10 23:29:15 +0700

    Clean props

--- src/app/components/InputStyles/index.js
+++ src/app/components/InputStyles/index.js
@@ -1,4 +1,5 @@
 // styles
+import React from "react"
 import styled from "styled-components"
 import { title, subtitle, reset } from "./styles"
 
@@ -6,8 +7,9 @@ import { title, subtitle, reset } from "./styles"
 import Textarea from "react-textarea-autosize"
 
 // return
-export const PlainTextarea = styled(Textarea)`${ reset }`
-export const TitleTextarea = styled(Textarea)`${ title }`
-export const SubtitleTextarea = styled(Textarea)`${ subtitle }`
-export const TitleInput = styled.input`${ title }`
-export const SubtitleInput = styled.input`${ subtitle }`
+// remove `warning` props from style HOCs
+export const PlainTextarea = styled( ({ warning, ...props }) => <Textarea { ...props } />)`${ reset }`
+export const TitleTextarea = styled( ({ warning, ...props }) => <Textarea { ...props } />)`${ title }`
+export const SubtitleTextarea = styled( ({ warning, ...props }) => <Textarea { ...props } />)`${ subtitle }`
+export const TitleInput = styled( ({ warning, ...props }) => <input { ...props } />)`${ title }`
+export const SubtitleInput = styled( ({ warning, ...props }) => <input { ...props } />)`${ subtitle }`
