9fe7da8db9e691a5dbbd01f2bec0e4bb666a3266
dmitrizzle <d@eurekaking.com>
2017-08-11 12:03:09 +0700

    Update link.js

--- src/constants/link.js
+++ src/constants/link.js
@@ -1,7 +1,3 @@
 
 export const API_DOMAIN = process.env.REACT_APP_API_URL
 export { DOMAIN_NAME } from "./app"
-
-export const API_DOMAIN = process.env.REACT_APP_API_URL
-export { DOMAIN_NAME } from "./app"
->>>>>>> bde5114ddaef330fca3837f2cdd69ad956a15c57
