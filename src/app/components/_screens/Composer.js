fec3c513205b00b84cce10fc4716db9aa49a96db
Dmitri <d@eurekaking.com>
2017-08-25 21:45:37 +0700

    Remove sign-in prompt from submit button at the bottom of Composer

--- src/app/components/_screens/Composer.js
+++ src/app/components/_screens/Composer.js
@@ -35,12 +35,10 @@ export default props => {
       <ModalDispatch
         with={{
           request: {
-            url: ROUTE_SUBMISSION_API,
-            data: composerState,
-            method: "POST",
-            headers: { Authorization: "JWT " + localStorage.getItem("token") }
+            url: "/api/messages/submit-consent.json"
           }
         }}
+        style={{ textDecoration: "none" }}
         wrapperElement="div"
       >
         <Button red>
