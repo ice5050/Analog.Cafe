034d7944d34b65b2b06907c424a6b15d7c747226
Dmitri <d@eurekaking.com>
2017-08-29 17:09:52 +0700

    Complete refactor

---
+++ src/app/components/Card/components/EditableProfile/index.js
@@ -0,0 +1,60 @@
+// tools
+import React from "react"
+
+// styles
+import { CardHeader } from "../../styles"
+import {
+  CardFlattened,
+  CardCaptionEditable,
+  CardButtonEditable
+} from "./styles"
+import { SubtitleInput } from "../../../InputStyles"
+import placeholder from "../../../_icons/images/placeholder-profile-upload.png"
+
+// constants
+import {
+  SUMMARY_LENGTH_MAX,
+  TITLE_LENGTH_OPTIMAL
+} from "../../../../../constants/input"
+
+// return
+export default props => {
+  return (
+    <CardFlattened>
+      <CardHeader>
+        <SubtitleInput
+          placeholder="Your Name"
+          value={props.title}
+          maxLength={TITLE_LENGTH_OPTIMAL}
+          autoFocus
+          onChange={event => props.changeTitle(event)}
+        />
+      </CardHeader>
+      <figure>
+        <img src={props.image || placeholder} alt="Profile avatar" />
+        {/* Image upload hidden input */}
+        <input
+          type="file"
+          accept="image/x-png,image/jpeg"
+          style={{ display: "none" }}
+        />
+
+        <figcaption>
+          <CardCaptionEditable
+            maxLength={SUMMARY_LENGTH_MAX}
+            placeholder="Your short author bio goes here."
+            onChange={event => props.changeText(event)}
+          >
+            {props.text}
+          </CardCaptionEditable>
+        </figcaption>
+      </figure>
+      <CardButtonEditable
+        value={props.buttons && props.buttons[1] && props.buttons[1].to}
+        placeholder="Website"
+        maxLength={SUMMARY_LENGTH_MAX}
+        onChange={event => props.changeButtons(event)}
+      />
+    </CardFlattened>
+  )
+}
