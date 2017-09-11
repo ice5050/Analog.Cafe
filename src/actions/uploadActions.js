db5350795788f721ab77cc0b0bf8b728d3136796
Dmitri <d@eurekaking.com>
2017-09-12 01:26:36 +0700

    Clean up

--- src/actions/uploadActions.js
+++ src/actions/uploadActions.js
@@ -67,36 +67,3 @@ export const send = request => {
       })
   }
 }
-
-// get user data matched to login credentials
-// export const getUser = token => {
-//   return dispatch => {
-//     axios({
-//       method: "get",
-//       headers: {
-//         Authorization: "JWT " + token
-//       },
-//       url: ROUTE_USER_API
-//     })
-//       .then(response =>
-//         dispatch({
-//           type: "USER.GET_USER",
-//           payload: response.data
-//         })
-//       )
-//       .catch(error =>
-//         dispatch(
-//           setCard(
-//             {
-//               status: "ok",
-//               info: {
-//                 title: errorMessage.VIEW_TEMPLATE.CARD.title,
-//                 text: errorMessage.DISAMBIGUATION.CODE_401.error
-//               }
-//             },
-//             { url: "errors/user" }
-//           )
-//         )
-//       )
-//   }
-// }
