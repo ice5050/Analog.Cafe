a1ba1c07f519e35e2f9fef34db718f1f5bb7a00f
Dmitri <d@eurekaking.com>
2017-09-11 17:31:31 +0700

    Plain button instead of link

--- src/app/containers/_screens-auth/SignIn/styles.js
+++ src/app/containers/_screens-auth/SignIn/styles.js
@@ -3,22 +3,21 @@ import React from "react"
 import styled, { css } from "styled-components"
 
 // components
-import { LinkButton } from "../../../components/Button"
+import { Button } from "../../../components/Button"
 import Twitter from "../../../components/_icons/components/Twitter"
 
 // css
 const brandButtonStyles = css`
-	color: 		${props => props.theme.color.background} !important;
-	svg {
-		height: 2em;
-	}
+  color: ${props => props.theme.color.background} !important;
+  svg {
+    height: 2em;
+  }
 `
-const StyledTwitterButton = styled(LinkButton)`
-	background-color:	#1da1f2;
-	${brandButtonStyles}
-	svg {
-		margin: -1.25em 0 -1em -1em;
-	}
+const StyledTwitterButton = styled(Button)`
+  background-color: #1da1f2;
+  ${brandButtonStyles} svg {
+    margin: -1.25em 0 -1em -1em;
+  }
 `
 
 export const TwitterButton = props => {
