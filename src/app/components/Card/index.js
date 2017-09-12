3e3710ca06a99a044c37a5cfa691d0bb8e6ab76f
Dmitri <d@eurekaking.com>
2017-09-12 14:00:14 +0700

    Fix conditional statement

--- src/app/components/Card/index.js
+++ src/app/components/Card/index.js
@@ -16,13 +16,15 @@ export const Card = props => {
       <CardHeader title={props.error && props.error}>
         <h3 onClick={event => event.stopPropagation()}>{props.title}</h3>
         {// Stubborn popup shows no close button but requires buttons within
-        !props.stubborn &&
-        props.buttons &&
-        Object.keys(props.buttons).length !== 0 && (
+        !(
+          props.stubborn &&
+          props.buttons &&
+          Object.keys(props.buttons).length !== 0
+        ) ? (
           <a href="#close" onClick={event => event.preventDefault()}>
             ✕
           </a>
-        )}
+        ) : null}
       </CardHeader>
       <figure onClick={event => event.stopPropagation()}>
         <img
