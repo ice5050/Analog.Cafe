8ffc37e5fac70371d6fed2b9403ce58662bffce0
Dmitri <d@eurekaking.com>
2017-09-26 12:19:04 +0700

    Responsive picture elements & url generator

--- src/app/components/Picture/components/PictureElement.js
+++ src/app/components/Picture/components/PictureElement.js
@@ -1,9 +1,10 @@
 // tools
 import React from "react"
+import { froth } from "../../../../utils/image-froth"
 
 // return
 export default props => {
-  const src = props.src
+  let src = props.src
   const className = props.className
 
   const classFeature = "Featured image"
@@ -18,14 +19,43 @@ export default props => {
       : classNofeature + by + props.author.name
   else alt = props.feature ? classFeature : classNofeature
 
+  let largestSize = props.feature ? "l" : "m"
+
   return (
     // feature and non-feature images should have different set of sizes.
     // non-feature image doesn't need huge size
     // NOTE for the list we can also use just medium images max & small images on mobile
-    <picture>
-      {/* <source srcset="smaller.jpg" media="(max-width: 768px)">
+    <div
+      style={{
+        paddingBottom: Math.round(100 / froth(src).ratio, 3) + "%",
+        background: "#EEE",
+        height: "0",
+        position: "relative"
+      }}
+    >
+      <picture
+        style={{
+          width: "100%",
+          height: "100%",
+          display: "block",
+          position: "absolute"
+        }}
+      >
+        {/* <source srcset="smaller.jpg" media="(max-width: 768px)">
         <source srcset="default.jpg"> */}
-      <img srcSet={src} src={src} alt={alt} className={className} />
-    </picture>
+        <source srcSet={froth(src, "s").src} media="(max-width: 480px)" />
+        <source srcSet={froth(src, "m").src} media="(max-width: 1200px)" />
+        <source
+          srcSet={froth(src, largestSize).src}
+          media="(min-width: 1201px)"
+        />
+        <img
+          src={froth(src, "m").src}
+          alt={alt}
+          className={className}
+          style={{ height: "100%" }}
+        />
+      </picture>
+    </div>
   )
 }
---
+++ src/utils/image-froth.js
@@ -0,0 +1,46 @@
+const IMAGE_FROTH_SERVER =
+  "https://res.cloudinary.com/analog-cafe/image/upload/"
+const IMAGE_FROTH_OPTIONS = "c_scale,fl_progressive"
+const IMAGE_FROTH_SIZES = {
+  t: "200",
+  s: "520",
+  m: "1268",
+  l: "1800"
+}
+
+// image-froth_1681956_9ad677d272a84ebc9360ad6199372f8b
+export const froth = (src, size = "m") => {
+  let width = IMAGE_FROTH_SIZES[size]
+  let height = ""
+  let ratio = 0
+  if (src.includes("image-froth") && !src.includes("/")) {
+    src =
+      IMAGE_FROTH_SERVER +
+      IMAGE_FROTH_OPTIONS +
+      ",w_" +
+      width +
+      "/" +
+      src +
+      ".jpg"
+    ratio =
+      src
+        .split("image-froth_")
+        .pop()
+        .split("_")
+        .shift() / 1000000
+    height = Math.round(width / ratio, 1)
+  }
+
+  console.log({
+    src,
+    ratio,
+    width,
+    height
+  })
+  return {
+    src,
+    ratio,
+    width,
+    height
+  }
+}
