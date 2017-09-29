e3324a090c02a77491059e8955b685026b176fd5
Dmitri <d@eurekaking.com>
2017-09-29 20:18:21 +0700

    Fix inset blank image error

--- src/app/containers/PictureDocket/index.js
+++ src/app/containers/PictureDocket/index.js
@@ -19,7 +19,37 @@ import { ModalDispatch } from "../Modal"
 // styles
 import { dot } from "../../components/_icons/components/BlankDot"
 
+// constants
 import { MESSAGE_HINT_IMAGE_SUGGESTIONS } from "../../../constants/messages/hints"
+const suggestions = [
+  "image-froth_915090_05378814ac7d4b9b9352b603f2d944de",
+  "image-froth_1546790_b5ff5d48edf8488387d39f64e18b2916",
+  "image-froth_1494432_2ed2035b7e154d6c88cb0280406f7193",
+  "image-froth_1522572_19174bdd522e4ab185e52d9d6fe9e868",
+  "image-froth_1542912_2fdd00455a0249c18bda84128470b341",
+  "image-froth_1515070_603a06c453204daa8983a81bbbeb2c63",
+  "image-froth_669104_8df1a40cea1746d79967ec4e694b59d9",
+  "image-froth_669104_a3b70899a1e74905bcb73b1e566943fc"
+]
+
+const GridButtonImage = props => {
+  return (
+    <GridButton onClick={() => props.add(props.src)}>
+      <AspectRatio>
+        <img
+          src={
+            froth({
+              src: props.src,
+              size: "t",
+              crop: "square"
+            }).src
+          }
+          alt="Editor’s suggestion"
+        />
+      </AspectRatio>
+    </GridButton>
+  )
+}
 
 // return
 export default class extends React.PureComponent {
@@ -50,6 +80,10 @@ export default class extends React.PureComponent {
         .apply()
     }
     editor.onChange(resolvedState)
+
+    // this helps refresh the view and update inserted image...
+    // ...i don't know why
+    window.scrollBy(0, 1)
   }
 
   // image upload handlers
@@ -73,8 +107,28 @@ export default class extends React.PureComponent {
         data: { file, key: key, src: dot }
       })
       .apply()
+
     editor.onChange(resolvedState)
     setTimeout(() => {
+      this.handleClose()
+    }, 10)
+  }
+
+  // insert selected image suggesstion:
+  handleImageSuggestion = src => {
+    const { editor } = this.props
+    const resolvedState = editor
+      .getState()
+      .transform()
+      .insertBlock({
+        type: "image",
+        isVoid: true,
+        data: { src }
+      })
+      .apply()
+
+    setTimeout(() => {
+      editor.onChange(resolvedState)
       this.handleClose()
     }, 10)
   }
@@ -90,24 +144,15 @@ export default class extends React.PureComponent {
         </CardHeader>
         <GridContainer>
           <GridRow>
-            <GridButton>
-              <AspectRatio>
-                <img
-                  src={
-                    froth({
-                      src:
-                        "image-froth_615281_5882b729360a465491c07860f6ec32e5",
-                      size: "t",
-                      crop: "square"
-                    }).src
-                  }
-                  alt=""
+            {suggestions.slice(0, 2).map(src => {
+              return (
+                <GridButtonImage
+                  key={src}
+                  src={src}
+                  add={this.handleImageSuggestion}
                 />
-              </AspectRatio>
-            </GridButton>
-            <GridButton>
-              <img src="/images/thumbnails/square.jpg" alt="" />
-            </GridButton>
+              )
+            })}
             <GridButton onClick={this.initFileUpload} red>
               <div style={{ margin: "0 auto" }}>
                 ＋
@@ -117,30 +162,26 @@ export default class extends React.PureComponent {
             </GridButton>
           </GridRow>
           <GridRow>
-            <GridButton>
-              <AspectRatio>
-                <img src="/images/thumbnails/square.jpg" alt="" />
-              </AspectRatio>
-            </GridButton>
-            <GridButton>
-              <img src="/images/thumbnails/square.jpg" alt="" />
-            </GridButton>
-            <GridButton>
-              <img src="/images/thumbnails/square.jpg" alt="" />
-            </GridButton>
+            {suggestions.slice(2, 5).map(src => {
+              return (
+                <GridButtonImage
+                  key={src}
+                  src={src}
+                  add={this.handleImageSuggestion}
+                />
+              )
+            })}
           </GridRow>
           <GridRow>
-            <GridButton>
-              <AspectRatio>
-                <img src="/images/thumbnails/square.jpg" alt="" />
-              </AspectRatio>
-            </GridButton>
-            <GridButton>
-              <img src="/images/thumbnails/square.jpg" alt="" />
-            </GridButton>
-            <GridButton>
-              <img src="/images/thumbnails/square.jpg" alt="" />
-            </GridButton>
+            {suggestions.slice(5, 8).map(src => {
+              return (
+                <GridButtonImage
+                  key={src}
+                  src={src}
+                  add={this.handleImageSuggestion}
+                />
+              )
+            })}
           </GridRow>
         </GridContainer>
         <GridCaption>
