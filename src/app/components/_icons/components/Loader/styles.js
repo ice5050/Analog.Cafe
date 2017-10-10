fe484f4883131a67c758b3dbb336f865ba413e1a
Dmitri <d@eurekaking.com>
2017-10-10 18:39:59 +0700

    Add files

---
+++ src/app/components/_icons/components/Loader/styles.js
@@ -0,0 +1,13 @@
+// styles
+import styled from "styled-components"
+export const LoaderContainer = styled.div`
+  display: inline-block;
+  overflow: hidden;
+  height: 1em;
+  svg {
+    width: 1em;
+    margin-top: -.1em;
+    margin-right: 0.25em;
+    margin-left: -.5em;
+  }
+`
